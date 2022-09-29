dcbb0022510e8f1e497a81a14a81094e86339e13
Peter Crona <petercrona89@gmail.com>
2022-09-29 07:59:10 +0200

    format

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ set_seed(42)
 
 res = generator("I eat data for breakfast", max_length=100, num_return_sequences=5)
 
-print(res)
+print('\n======================================\n')
+
+for x in res:
+    print(x['generated_text'])
+    print('\n')
+
+print('\n======================================\n')
